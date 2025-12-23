64e3692aa8b7c7b606c54f1e88083edd1e76610c
Jobayer75368 <jobayer30325@gmail.com>
2025-12-23 16:08:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    $("#main_form").submit(function (e) {
+        e.preventDefault()
+
+        $inputNameValue = $(".name").val()
+        $inputEmailValue = $(".email").val()
+        $inputSubjectValue = $(".subject").val()
+        $TextareaValue = $("textarea").val()
+        let isValue = true;
+
+
+        if ($inputNameValue == "") {
+            $(".name_alert").text("Please fill out the name field!")
+            isValue = false
+        }
+        else {
+            $(".name_alert").text("")
+        }
+
+        if ($inputEmailValue == "") {
+            $(".email_alert").text("Please fill out the email field!")
+            isValue = false
+        }
+        else {
+            $(".email_alert").text("")
+        }
+
+        if ($inputSubjectValue == "") {
+            $(".subject_alert").text("Please fill out the subject field!")
+            isValue = false
+        }
+        else {
+            $(".subject_alert").text("")
+        }
+
+        if ($TextareaValue == "") {
+            $(".msg_alert").text("Please fill out the message field!")
+            isValue = false
+        }
+        else {
+            $(".msg_alert").text("")
+        }
+
+        if (isValue) {
+            alert(` Name: ${$inputNameValue} \n Email: ${$inputEmailValue} \n Subject: ${$inputSubjectValue}\n Message: ${$TextareaValue}`)
+
+            $(".name").val("")
+            $(".email").val("")
+            $(".subject").val("")
+            $("textarea").val("")
+        }
+
+    })
+
+});
